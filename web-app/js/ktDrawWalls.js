622aeefb885cf5345edb3a49f9155fd7a6525b79
corinnekrych <corinnekrych@gmail.com>
2013-08-16 21:56:13 +0200

    minecraft effect for walls

--- web-app/js/ktDrawWalls.js
+++ web-app/js/ktDrawWalls.js
@@ -29,75 +29,79 @@
 // </code></pre>
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(factory);
-  } else {
-    root.ktDrawWalls = factory();
-  }
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else {
+        root.ktDrawWalls = factory();
+    }
 }(this, function () {
 
-  return function (canvas, config, initial) {
-    var ctx = canvas.getContext('2d');
-    var width = canvas.width;
-    var height = canvas.height;
-    var wstep = width / (config.grid + 1);
-    var hstep = height / (config.grid + 1);
-    var current = initial;
-    var paused = true;
+    return function (canvas, config, initial) {
+        var ctx = canvas.getContext('2d');
+        var width = canvas.width;
+        var height = canvas.height;
+        var wstep = width / (config.grid + 1);
+        var hstep = height / (config.grid + 1);
+        var current = initial;
 
-    // Launch images loading in parallel
-    var images = {};
-    var src = {};
-    var fetchImage = function (file) {
-      src[file] = new Image();
-      src[file].onload = function () {
-        if (paused) {
-          animate();
+        // Drawing
+        var clean = function () {
+            ctx.clearRect(0, 0, width, height);
+        };
+
+        function computeColor(color, intensity) {
+            return "rgb(" + computeIntensity(color, 0, intensity) + ", " + computeIntensity(color, 1, intensity) + ", " + computeIntensity(color, 2, intensity) + ")";
         }
-      };
-      src[file].src = 'images/game/' + file;
-    };
-    var fetchImages = function (imgs) {
-      for (var name in imgs) {
-        if (imgs.hasOwnProperty(name)) {
-          if (!src[imgs[name]]) {
-            fetchImage(imgs[name]);
-          }
-          images[name] = src[imgs[name]];
+
+        function computeIntensity(color, idx, intensity) {
+            return Math.floor(color.min[idx] + ((color.max[idx] - color.min[idx]) * intensity));
         }
-      }
+        var drawWall = function (x, y, rotation) {
+            var pixels = 150;
+            var green = {
+                min: [55, 90, 36],
+                max: [105, 170, 70]
+            };
+            var grid = 15;
+            var centerx = x * pixels / grid;
+            var centery = y * pixels / grid;
+            var size = 5;
+            for (var i = 0; i < pixels; i++) {
+                for (var j = 0; j < pixels; j++) {
+                    var distance = Math.sqrt((i - centerx) * (i - centerx) + (j - centery) * (j - centery));
+                    if (distance < size || (distance < 2 * size && Math.random() > (distance - size) / size)) {
+                        var intensity = Math.random();
+                        ctx.fillStyle = computeColor(green, intensity);
+                        ctx.fillRect (i, j, 1, 1);
+                    }
+                }
+            }
+
+
+        };
+
+        // Animate from frame to frame
+        var animate = function () {
+            var name;
+            var item;
+            clean();
+            var pixels = 100;
+            ctx.save();
+//            ctx.scale(canvas.width / pixels, canvas.height / pixels);
+            ctx.scale(7,7);
+
+            for (name in current) {
+                if (current.hasOwnProperty(name)) {
+                    item = current[name];
+                    drawWall(item.x, item.y, 0);
+                }
+            }
+
+            ctx.restore();
+        };
+
+        // Draw initial frame
+        animate();
+
     };
-    fetchImages(config.images);
-
-    // Drawing
-    var clean = function () {
-      ctx.clearRect(0, 0, width, height);
-    };
-
-    var drawImage = function (name, x, y, rotation) {
-      ctx.save();
-      ctx.translate((x + 1) * wstep, (config.grid - y) * hstep);
-      ctx.rotate(rotation);
-      ctx.drawImage(images[name], -wstep/2, -hstep/2, wstep, hstep);
-      ctx.restore();
-    };
-
-    // Animate from frame to frame
-    var animate = function () {
-      var name;
-      var item;
-      clean();
-
-      for (name in current) {
-        if (current.hasOwnProperty(name)) {
-          item = current[name];
-          drawImage(name, item.x, item.y, 0);//Can handle rotation too
-        }
-      }
-    };
-
-    // Draw initial frame
-    animate();
-
-  };
 }));
