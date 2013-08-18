192a48a199f21a0b91cf284e2ed04260df6565c2
corinnekrych <corinnekrych@gmail.com>
2013-08-19 00:02:07 +0200

    Update configuration-bootstrap.js

--- web-app/js/kissingturtles/configuration-bootstrap.js
+++ web-app/js/kissingturtles/configuration-bootstrap.js
@@ -2,8 +2,8 @@ var kissingturtles = kissingturtles || {};
 
 kissingturtles.loadConfiguration = (function () {
     kissingturtles.configuration = {
-    //baseURL: "http://localhost:8080/KissingTurtles/",
-    baseURL: "http://192.168.0.13:8080/KissingTurtles/",
+    baseURL: "http://localhost:8080/KissingTurtles/",
+    //baseURL: "http://192.168.0.13:8080/KissingTurtles/",
     //baseURL: "http://KissingTurtles.cloudfoundry.com/",
     namespace: "kissingturtles",
     domain:[]
